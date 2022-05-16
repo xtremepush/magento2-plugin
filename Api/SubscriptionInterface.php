84420efe3139538e90a8db75e1103b80d307682a
Mindaugas Degutis <mindaugas.degutis@alchemetrics.co.uk>
2022-05-16 16:14:26 +0300

    XP-4503 Unsub/sub API route and model logic

---
+++ Api/SubscriptionInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Xtremepush\Module\Api;
+
+interface SubscriptionInterface
+{
+    /**
+     * Handle Xtremepush subscription statuses on Magento shop side
+     *
+     * @param int $customerId
+     * @param bool $status
+     * @param int $storeId
+     * @return bool
+     */
+    public function handleSubscriptionStatus(int $customerId, bool $status, int $storeId);
+}
