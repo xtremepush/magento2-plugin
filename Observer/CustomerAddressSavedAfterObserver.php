b231bc25ca13882e5f4143c72ff2529c2311610e
Augustas Trumpa <augustas.trumpa@xtremepush.com>
2022-08-02 17:52:42 +0300

    XP-5138 - fixed address missing from payload

--- Observer/CustomerAddressSavedAfterObserver.php
+++ Observer/CustomerAddressSavedAfterObserver.php
@@ -8,6 +8,7 @@ use Psr\Log\LoggerInterface;
 use Xtremepush\Module\Helper\WebhookService;
 use Xtremepush\Module\Model\Event;
 use Xtremepush\Module\Model\ModuleConfig;
+use Xtremepush\Module\Serialize\CustomerAddressesSerializer;
 use Xtremepush\Module\Serialize\CustomerSerializer;
 
 class CustomerAddressSavedAfterObserver extends AbstractObserver
@@ -15,14 +16,19 @@ class CustomerAddressSavedAfterObserver extends AbstractObserver
     /** @var CustomerSerializer */
     private $customerSerializer;
 
-   public function __construct(
+    /** @var CustomerAddressesSerializer */
+    private $addressesSerializer;
+
+    public function __construct(
         LoggerInterface $logger,
         WebhookService $webhookService,
         Session $authSession,
         ModuleConfig $config,
-        CustomerSerializer $customerSerializer
+        CustomerSerializer $customerSerializer,
+        CustomerAddressesSerializer $addressesSerializer
     ) {
         $this->customerSerializer = $customerSerializer;
+        $this->addressesSerializer = $addressesSerializer;
         parent::__construct($logger, $webhookService, $authSession, $config);
     }
 
@@ -34,8 +40,21 @@ class CustomerAddressSavedAfterObserver extends AbstractObserver
             return $this;
         }
 
+        $addresses = $customer->getAddresses();
+        if (empty($addresses)) {
+            $customer = $this->customerSerializer->toArray($customer->getDataModel());
+            $address = $address->getData();
+            $customer['addresses'][] = $this->addressesSerializer->remapArray($address);
+            if ($address['is_default_billing'] == '1') {
+                $customer['default_billing'] = $address['id'];
+            }
+            if ($address['is_default_shipping'] == '1') {
+                $customer['default_shipping'] = $address['id'];
+            }
+        }
+
         if ($this->isWebhookEnabled(Event::EVENT_CUSTOMER_CUSTOMER_UPDATED)) {
-            $this->webhookService->sendWebhook(Event::EVENT_CUSTOMER_CUSTOMER_UPDATED, $this->customerSerializer->toArray($customer->getDataModel()));
+            $this->webhookService->sendWebhook(Event::EVENT_CUSTOMER_CUSTOMER_UPDATED, $customer);
         }
 
         return $this;
--- Serialize/CustomerAddressesSerializer.php
+++ Serialize/CustomerAddressesSerializer.php
@@ -34,4 +34,27 @@ class CustomerAddressesSerializer
 
         return $data;
     }
+
+    /**
+     * @param array $address
+     * @return array
+     */
+    public function remapArray(array $address)
+    {
+        return [
+            'id' => $address['id'] ?? '',
+            'customer_id' => $address['customer_id'] ?? '',
+            'region' => $address['region'] ?? '',
+            'region_id' => $address['region_id'] ?? '',
+            'country_id' => $address['country_id'] ?? '',
+            'street' => $address['street'] ?? '',
+            'telephone' => $address['telephone'] ?? '',
+            'postcode' => $address['postcode'] ?? '',
+            'city' => $address['city'] ?? '',
+            'firstname' => $address['firstname'] ?? '',
+            'lastname' => $address['lastname'] ?? '',
+            'default_shipping' => $address['is_default_shipping'] ?? '',
+            'default_billing' => $address['is_default_billing'] ?? ''
+        ];
+    }
 }
